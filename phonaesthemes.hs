cf6a49311c587dce0e6b605f1a39c0692f8a03f6
H. Chase Stevens <chase@chasestevens.com>
2014-09-10 03:31:07 +0100

    add wilson score interval calculation

--- phonaesthemes.hs
+++ phonaesthemes.hs
@@ -10,6 +10,7 @@ import System.IO (withFile, IOMode(AppendMode), hPutStrLn)
 import Control.Monad (forever, when)
 
 
+-- Edges
 data Edge = Edge String String String 
 	deriving (Show, Read)  --TODO: minified instancing of show/read
 
@@ -49,6 +50,7 @@ minify edge = Edge [] (removePrefix $ start edge) (removePrefix $ end edge)
 			| otherwise = str
 
 
+-- String processing
 (prefixMin, prefixMax) = (2, 4)
 
 prefixes :: String -> [String]
@@ -73,19 +75,48 @@ ngramPairs :: Edge -> [(String, String)]
 ngramPairs edge = [(ngram, end edge) | ngram <- ngrams edge]
 
 
+-- Counters
 type Counter a = M.Map a Int
 
 updateCount :: (Ord a) => a -> Counter a -> Counter a
 updateCount k dict = M.insertWith (+) k 1 dict
 
 populateCounts :: (Counter (String, String), Counter String, Counter String) -> (String, String) -> (Counter (String, String), Counter String, Counter String)
-populateCounts (assoc_dict, ngram_dict, meaning_dict) pair@(ngram, meaning) = (assoc_dict', ngram_dict', meaning_dict')
-	where
-		ngram_dict' = updateCount ngram $! ngram_dict
+populateCounts (assoc_dict, ngram_dict, meaning_dict) pair@(ngram, meaning) = let
 		assoc_dict' = updateCount pair $! assoc_dict
-		meaning_dict' = updateCount meaning $! meaning_dict
+		ngram_dict' = updateCount ngram $! ngram_dict
+		meaning_dict' = updateCount meaning $! meaning_dict 
+	in (assoc_dict', ngram_dict', meaning_dict')
 
 
+-- Statistics
+data IntervalBounds = IntervalBounds Float Float
+	deriving (Show)
+
+highBound (IntervalBounds x _) = x
+lowBound (IntervalBounds _ x) = x
+
+wilsonBounds :: Int -> Int -> IntervalBounds
+wilsonBounds n 1 = IntervalBounds 1 0
+wilsonBounds n total = IntervalBounds upper lower
+	where
+		z = 2.575  -- 99% confidence
+		n' = fromIntegral n
+		total' = fromIntegral total
+		phat = n' / total'
+		a = phat + z * z / (2 * total')
+		b = z * sqrt ((phat * (1 - phat) + z * z / (4 * total')) / total')
+		denom = 1 + z * z / total'
+		upper = (a + b) / denom
+		lower = (a - b) / denom
+
+lowerProb :: (Ord k) => Counter k -> (a, k) -> Int -> (Float, Int, Int)
+lowerProb total_counts (_, k) n = (lowBound $ wilsonBounds n total, n, total)
+	where
+		total = fromJust $ k `M.lookup` total_counts
+
+
+-- I/O
 simedge_cache = ".simedge_cache"
 cache = ".cache"
 
@@ -111,23 +142,26 @@ main = do
 	conts <- getDirectoryContents "assertions"
 	let csv_fnames = map ((++) "assertions\\") $ filter (isSuffixOf "csv") conts
 	-- TODO: discard caches if assertions have been added/removed
+	-- Populate cache, if necessary:
 	when (any (not . (flip elem) ls) [simedge_cache, cache]) (do
 		when (simedge_cache `elem` ls) (removeFile simedge_cache)
 		when (cache `elem` ls) (removeFile cache)
 		mapM_ preprocess csv_fnames)
 	simedges <- readFile simedge_cache
 	-- TODO: ngramPairs optionally returns phonemes (SPHINX?)
+	-- Build counts:
 	let pairs = concat $ filter (not . null) $ map (ngramPairs . read) $ lines simedges
 	let (assoc_counts, ngram_totals, meaning_totals) = foldl' populateCounts (M.empty, M.empty, M.empty) pairs
 	-- TODO: Further populate with depth-2 concepts (loaded in from .cache)
 	-- TODO: Filter by not "matches"
 	-- TODO: Lower bound of wilson score on concepts
+	let assoc_probs = M.mapWithKey (lowerProb meaning_totals) assoc_counts
 	putStrLn "Ready."
 	-- Mode 1: Interactive, user supplies prefixes or suffixes
 	forever $ do
 		term <- getLine
-		let keys = filter (((==) term) . fst) $ M.keys assoc_counts
-		let results = map (\key -> (snd key, fromJust $ M.lookup key assoc_counts)) keys
+		let keys = filter (((==) term) . fst) $ M.keys assoc_probs
+		let results = map (\key -> (snd key, fromJust $ M.lookup key assoc_probs)) keys
 		let sorted_results = sortBy (compare `on` snd) results
 		mapM_ (putStrLn . show) sorted_results
 	-- TODO: Mode 2: 
